1862021fae294b1b2b7e46914d2d1d1872a3c35c
shahin1290 <s.patowary@yahoo.com>
2019-05-09 13:42:48 +0200

    Sets up routes by using express router

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express()
 // Connect Database
 connectDB()
 
-app.get('/', (req, res) => {
-  res.send('index')
-})
+// Define Routes
+app.use('/api/users', require('./config//routes/api/users'))
+app.use('/api/auth', require('./config/routes/api/auth'))
+app.use('/api/profile', require('./config/routes/api/profile'))
+app.use('/api/groups', require('./config/routes/api/groups'))
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => { console.log(`Server started on port ${PORT}`)})
