4d080f482f5b8da6121369c667ae5ec7bdc4e5ef
shahin1290 <s.patowary@yahoo.com>
2019-05-16 18:36:32 +0200

    Sets up react router with auth components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Lnading from './components/layout/Landing'
 import './App.css';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const  App = () => 
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing}/>
 
-  <Fragment>
-    <Navbar />
-    <Lnading />
-  </Fragment>
+      <section>
+        <Switch>
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/login' component={Login}/>
+
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
   
 
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// import Logout from '../../components/Auth/Logout'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
-    );
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  icon: {
+    textDecoration: 'none',
+    color: 'white',
+    fontWeight: 'bold'
   }
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  let signUpLink, logInLink, logOutLink, createGroupLink, profileLink
+
+    createGroupLink =  <Button  color="inherit">Create a Group</Button>;
+    // logOutLink = <Logout />
+    profileLink = <Button color="inherit">Profile</Button>;
+
+    signUpLink =  <Button component={Link} to="/register" color="inherit">SignUp</Button>;
+    logInLink =  <Button component={Link} to="/login" color="inherit">LogIn</Button>
+  
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.grow}>
+            WeMeet
+          </Typography>
+          { signUpLink }
+          { logInLink }
+          { createGroupLink }
+          { logOutLink }
+          { profileLink }
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Navbar;
+// const mapStateToProps = (state) => {
+//   return {
+//     isSignedIn: state.reduxTokenAuth.currentUser.isSignedIn,
+//     currentUser: state.reduxTokenAuth.currentUser
+//   }
+// }
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(ButtonAppBar);
