fd3b199faf444871fd07374b2d17570faed6ff28
shahin1290 <s.patowary@yahoo.com>
2019-05-17 18:25:48 +0200

    Creates redux store, alert reducers, action and types]

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import { Provider } from 'react-redux'
+import store from './store'
 
 const  App = () => 
+<Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
@@ -21,7 +24,6 @@ const  App = () =>
       </section>
     </Fragment>
   </Router>
-  
-
+</Provider>
 
 export default App;
