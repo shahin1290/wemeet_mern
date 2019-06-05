1de9fcecba6a8439497367cdf2d6ca2163ed1c83
shahin1290 <s.patowary@yahoo.com>
2019-06-05 23:46:09 +0200

    Connects group and auth state

--- client/src/components/groups/GroupItem.js
+++ client/src/components/groups/GroupItem.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-const GroupItem = props => {
+const GroupItem = ({auth, group: {name, description, date, location, user}}) => {
   return (
     <div>
-      
+      <p>{name}</p>
+      <p>{description}</p>
+      <p>{date}</p>
+      <p>{location}</p>
+      <p>{user}</p>
     </div>
   );
 };
 
 GroupItem.propTypes = {
-  
+  group: PropTypes.object.isRequired
 }
 
-export default GroupItem;
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {})(GroupItem);
